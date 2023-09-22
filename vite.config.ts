c504316fe715bc7a1aa6dd264c6053f099e46286
Matheus Mascarenhas de Barros <matheusmbarros01@gmail.com>
2023-09-22 00:06:07 -0300

    viteNew

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), { name: "@vitejs/plugin-react" }],
-	build: {
-		rollupOptions: {
-			external: ["react", "react-dom", "react-router-dom", "react-router"],
-		},
-	},
 });
