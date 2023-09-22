bb4c27314719677c3331ddb6585622a155a5ccfc
Matheus Mascarenhas de Barros <matheusmbarros01@gmail.com>
2023-09-22 01:26:20 -0300

    Molding

--- src/pages/Molding/Molding.tsx
+++ src/pages/Molding/Molding.tsx
@@ -105,7 +105,7 @@ const ProductionOrder: React.FC = () => {
 		const year = date.getFullYear();
 		const month = (date.getMonth() + 1).toString().padStart(2, "0");
 		const day = date.getDate().toString().padStart(2, "0");
-		const hours = date.getHours().toString().padStart(2, "0");
+		const hours = (date.getHours() - 3).toString().padStart(2, "0");
 		const minutes = date.getMinutes().toString().padStart(2, "0");
 		return `${year}-${month}-${day}T${hours}:${minutes}`;
 	};
